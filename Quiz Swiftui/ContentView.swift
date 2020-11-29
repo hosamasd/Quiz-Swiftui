75720695eb8e04584a50d60d95576b3603cc350e
hosam mohamed <hosamelmalt@gmail.com>
2020-11-29 10:33:17 -0800

    Initial Commit

---
+++ Quiz Swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quiz Swiftui
+//
+//  Created by Hossam on 11/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quiz Swiftui/Quiz_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_SwiftuiApp.swift
+//  Quiz Swiftui
+//
+//  Created by Hossam on 11/29/20.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
